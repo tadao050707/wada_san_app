5aee85c3424a12314104e4e6ebc6944e31c0cfc1
KW <kw@KWnoMacBook-Pro.local>
2022-01-01 12:14:50 +0900

    UserとAdminモデルのマイグレーションファイルを修正してmigrate

--- db/migrate/20220101023455_devise_create_users.rb
+++ db/migrate/20220101023455_devise_create_users.rb
@@ -15,11 +15,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -32,14 +32,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name
-      t.string :phone_number
-      t.string :nickname
+      t.string :name, null: false
+      t.string :phone_number, null: false
+      t.string :nickname, null: false
       t.references :company
 
       t.timestamps null: false
     end
 
+    add_index :users, :phone_number,         unique: true
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
