22fd616d89f804f76dd0fed98687bcdc164fd250
KW <kw@KWnoMacBook-Pro.local>
2022-01-01 13:12:14 +0900

    Groupingモデルを作成, migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_01_023543) do
+ActiveRecord::Schema.define(version: 2022_01_01_040928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2022_01_01_023543) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "groupings", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_groupings_on_company_id"
+    t.index ["user_id"], name: "index_groupings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,4 +74,6 @@ ActiveRecord::Schema.define(version: 2022_01_01_023543) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groupings", "companies"
+  add_foreign_key "groupings", "users"
 end
