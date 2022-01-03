84da9a96d7fc529f53c04405bfec4c2316376cf5
KW <kw@KWnoMacBook-Pro.local>
2022-01-03 13:52:10 +0900

    drop_tableのマイグレーションファイルが削除できず、rails db:dropを実行して削除。ProductModelを再び生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_01_040928) do
+ActiveRecord::Schema.define(version: 2022_01_03_044705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,20 @@ ActiveRecord::Schema.define(version: 2022_01_01_040928) do
     t.index ["user_id"], name: "index_groupings_on_user_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.bigint "code", null: false
+    t.string "name", null: false
+    t.text "remarks"
+    t.integer "regular_price"
+    t.decimal "selling_price"
+    t.decimal "cost_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["code"], name: "index_products_on_code", unique: true
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -76,4 +90,5 @@ ActiveRecord::Schema.define(version: 2022_01_01_040928) do
 
   add_foreign_key "groupings", "companies"
   add_foreign_key "groupings", "users"
+  add_foreign_key "products", "users"
 end
