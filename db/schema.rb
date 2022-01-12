2cbb5abb3a296b04ff274dece495d76b72bc205c
KW <kw@KWnoMacBook-Pro.local>
2022-01-12 18:18:40 +0900

    QuestionModelを作成、migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_105322) do
+ActiveRecord::Schema.define(version: 2022_01_12_091621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,15 @@ ActiveRecord::Schema.define(version: 2022_01_07_105322) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.bigint "code", null: false
     t.string "name", null: false
@@ -114,4 +123,5 @@ ActiveRecord::Schema.define(version: 2022_01_07_105322) do
   add_foreign_key "groupings", "teams"
   add_foreign_key "groupings", "users"
   add_foreign_key "products", "users"
+  add_foreign_key "questions", "users"
 end
