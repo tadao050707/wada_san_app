672e7a15d9f1325385033307d228a662138063f3
KW <kw@KWnoMacBook-Pro.local>
2022-01-19 12:27:04 +0900

    create_teamsのマイグレファイルにt.boolean :chief, null: false, default: falseを追記

--- db/migrate/20220101020854_create_teams.rb
+++ db/migrate/20220101020854_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration[6.1]
     create_table :teams do |t|
       t.string :name, null: false
       t.text :remarks
+      t.boolean :chief, null: false, default: false
 
       t.timestamps
     end
