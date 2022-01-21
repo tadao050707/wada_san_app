d02efd802ecdd5b4dc9775e1a9795d5811bbbcc7
KW <kw@KWnoMacBook-Pro.local>
2022-01-21 21:51:03 +0900

    products_helper.rbにdef integer_or_floatを定義

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def integer_or_float
+    if @product.selling_price.to_s[-2..-1] == ".0"
+      @product.selling_price.to_i
+    else
+      @product.selling_price
+    end
+  end
 end
