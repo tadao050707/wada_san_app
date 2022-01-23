2f1d21e921848b27e2005f7bc51231b78159807f
KW <kw@KWnoMacBook-Pro.local>
2022-01-23 10:44:14 +0900

    bin/rails db:migrate RAILS_ENV=developmentを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_233217) do
+ActiveRecord::Schema.define(version: 2022_01_23_005828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,16 @@ ActiveRecord::Schema.define(version: 2022_01_22_233217) do
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
+  create_table "sale_details", force: :cascade do |t|
+    t.bigint "quantity"
+    t.bigint "sale_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_sale_details_on_product_id"
+    t.index ["sale_id"], name: "index_sale_details_on_sale_id"
+  end
+
   create_table "sales", force: :cascade do |t|
     t.bigint "code", null: false
     t.date "date_at", null: false
@@ -150,6 +160,8 @@ ActiveRecord::Schema.define(version: 2022_01_22_233217) do
   add_foreign_key "groupings", "users"
   add_foreign_key "products", "users"
   add_foreign_key "questions", "users"
+  add_foreign_key "sale_details", "products"
+  add_foreign_key "sale_details", "sales"
   add_foreign_key "sales", "clients"
   add_foreign_key "sales", "users"
   add_foreign_key "teams", "users"
