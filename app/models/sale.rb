28c0adec7eaeec51eace8d291925015b81c6d177
KW <kw@KWnoMacBook-Pro.local>
2022-01-27 23:53:56 +0900

    sale.rbにaccepts_nested_attributes_for :sale_details, allow_destroy: trueを追記
    sales/form.htmlを編集中

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,6 @@
 class Sale < ApplicationRecord
   belongs_to :client
   belongs_to :user
-  has_many :sale_details
+  has_many :sale_details, dependent: :destroy
+  accepts_nested_attributes_for :sale_details, allow_destroy: true
 end
