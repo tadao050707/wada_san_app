0b423540f5380d892b609b7d71502e47fc5d177f
KW <kw@KWnoMacBook-Pro.local>
2022-01-29 00:15:58 +0900

    teamsテーブルchiefカラムのデータ型をintegerに変更

--- db/migrate/20220101020854_create_teams.rb
+++ db/migrate/20220101020854_create_teams.rb
@@ -3,7 +3,7 @@ class CreateTeams < ActiveRecord::Migration[6.1]
     create_table :teams do |t|
       t.string :name, null: false
       t.text :remarks
-      t.boolean :chief, null: false, default: false
+      t.integer :chief, null: false
 
       t.timestamps
     end
