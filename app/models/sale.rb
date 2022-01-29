e4e5202a16e4a05451795abdd11b02f0f3048d91
KW <kw@KWnoMacBook-Pro.local>
2022-01-29 17:27:13 +0900

    sale.rbにhas_many :saled_products, through: :sale_details, source: :productを追記

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,5 +2,6 @@ class Sale < ApplicationRecord
   belongs_to :client
   belongs_to :user
   has_many :sale_details, dependent: :destroy
+  has_many :saled_products, through: :sale_details, source: :product
   accepts_nested_attributes_for :sale_details, allow_destroy: true
 end
